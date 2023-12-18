06a1ad71701f0d579384a592dc09c7a50b3cbaf9
Sagi Halevy <sagi1345@gmail.com>
2023-12-18 13:27:39 +0200

    added the server

---
+++ mobile-remote-server/mobile-remote-server/ConnectionHub.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace mobile_remote_server
+{
+    public class ConnectionHub : Hub
+    {
+        private static readonly Dictionary<string, List<string>> RoomUsers = new Dictionary<string, List<string>>();
+
+        public async Task CreateRoom() {
+
+            var roomId = Guid.NewGuid().ToString("N");
+            await Console.Out.WriteLineAsync($"Room Created: {roomId}");
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+            RoomUsers[roomId] = new List<string> { Context.ConnectionId };
+
+            await Clients.Caller.SendAsync("ReceiveRoomId", roomId);
+        }
+
+
+        public async Task JoinRoom(string roomId)
+        {
+            if (!string.IsNullOrEmpty(roomId) && RoomUsers.TryGetValue(roomId, out var users))
+            {
+                if (users.Count < 2)
+                {
+                    users.Add(Context.ConnectionId);
+                    await Groups.AddToGroupAsync(Context.ConnectionId, roomId);
+                    Console.WriteLine($"User joined room: {roomId}");
+                    await Clients.All.SendAsync("ReceiveSuccessJoin", roomId);
+                    await Clients.All.SendAsync("ControllerSuccessJoin");
+                    
+                }
+                else
+                {
+                    Console.WriteLine($"Room {roomId} is already full.");
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Room does not exist or is invalid: {roomId}");
+            }
+        }
+
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            await Console.Out.WriteLineAsync(message);
+        }
+
+        public async Task SendOrientation(object orientationData)
+        {
+   
+            await Console.Out.WriteLineAsync("something orieneteiaton");
+            await Console.Out.WriteLineAsync(orientationData.ToString());
+            await Clients.All.SendAsync("ReceiveOrientation", orientationData);
+        }
+
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            // Log the exception
+            Console.WriteLine($"Client disconnected with error: {exception?.Message}");
+
+            await base.OnDisconnectedAsync(exception);
+        }
+
+    }
+}
---
+++ mobile-remote-server/mobile-remote-server/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.DependencyInjection;
+using mobile_remote_server;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Cors;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add SignalR services to the service collection
+builder.Services.AddSignalR();
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(builder =>
+        builder.SetIsOriginAllowed(origin => true) // Allow any origin
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .AllowCredentials()); // Allow credentials
+});
+
+var app = builder.Build();
+app.UseCors();
+
+app.MapHub<ConnectionHub>("/orientationHub");
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
+
+
